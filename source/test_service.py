04224f24da4eacfc3ea468c467a5478e20afb934
Your-vo1d <my.vo1d.edge@gmail.com>
2024-05-23 22:57:31 +0700

    Added test for GET/information

--- source/test_service.py
+++ source/test_service.py
@@ -1,5 +1,5 @@
 import requests
-
+import pytest
 # URL
 base_url = "http://127.0.0.1:8000"
 
@@ -20,6 +20,8 @@ invalid_user = {
 }
 
 # Тест для входа пользователя
+
+
 def test_login():
     # Отправляем запрос на вход с правильными учетными данными
     response = requests.post(f"{base_url}/login", json=valid_user)
@@ -44,5 +46,32 @@ def test_login():
     assert "detail" in data
     assert data["detail"] == "User not found"
 
-if __name__ == "__main__":
-    test_login()
+
+@pytest.fixture(scope="module")
+def secret_key():
+    responce = requests.post(f"{base_url}/login", json=valid_user)
+    data = responce.json()
+    return data["secret_key"]
+
+
+def test_information(secret_key):
+    response = requests.get(f"{base_url}/information",
+                            params={"secret_code": secret_key})
+    assert response.status_code == 200
+    data = response.json()
+    assert "username" in data
+    assert "salary" in data
+    assert "promotion_date" in data
+
+    # Выводим информацию о пользователе в терминал
+    print("Имя пользователя:", data["username"])
+    print("Зарплата:", data["salary"])
+    print("Дата повышения:", data["promotion_date"])
+
+    # Отправляем запрос на получение информации с неправильным секретным кодом
+    response = requests.get(f"{base_url}/information",
+                            params={"secret_code": "invalid_secret"})
+    assert response.status_code == 404
+    data = response.json()
+    assert "detail" in data
+    assert data["detail"] == "Invalid or expired secret code"
