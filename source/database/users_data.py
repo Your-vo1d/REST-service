c92cfada84199341e1cefeb9d6d796c6b253fb4e
Your-vo1d <my.vo1d.edge@gmail.com>
2024-05-25 20:44:42 +0700

    Initial commit: created examples of users

---
+++ source/database/users_data.py
@@ -0,0 +1,15 @@
+valid_user = {
+    "username": "testuser",
+    "password": "testpassword",
+    "secret_code": "0fbd35e9405c601d4dcc8e12d0a1651d",
+    "salary": 50000,
+    "promotion_date": "2023-12-31"
+}
+
+invalid_user = {
+    "username": "invaliduser",
+    "password": "wrongpassword",
+    "secret_code": "wrongsecret",
+    "salary": 60000.0,
+    "promotion_date": "2022-12-31"
+}
