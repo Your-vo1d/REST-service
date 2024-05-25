3f24ec46778ce08e772310e8bd9236b03b70f380
Your-vo1d <my.vo1d.edge@gmail.com>
2024-05-25 20:33:25 +0700

    Update commit: wrapped in a function (set_database)

--- source/database/database_set.py
+++ source/database/database_set.py
@@ -1,24 +1,40 @@
 import sqlite3
 from source import DATABASE_PATH
 from source.models import User
-
+from users_data import valid_user
 
 def set_database(user: User):
-    # Подключение к базе данных
-    conn = sqlite3.connect(DATABASE_PATH)
-    cursor = conn.cursor()
+    try:
+        # Подключение к базе данных
+        conn = sqlite3.connect(DATABASE_PATH)
+        cursor = conn.cursor()
 
-    # Вставка данных первого пользователя в таблицу
-    cursor.execute('''INSERT INTO users 
-                    (username, password, secret_code, salary, promotion_date) 
-                    VALUES (?, ?, ?, ?, ?)''',
-                   (user.username,
-                    user.password,
-                    user.secret_code,
-                    user.salary,
-                    user.promotion_date))
-    # Сохранение изменений
-    conn.commit()
+        # Проверка существования таблицы users
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users';")
+        if cursor.fetchone() is None:
+            raise Exception("Таблица 'users' не существует в базе данных.")
 
-    # Закрытие соединения
-    conn.close()
+        # Вставка данных первого пользователя в таблицу
+        cursor.execute('''INSERT INTO users 
+                        (username, password, secret_code, salary, promotion_date) 
+                        VALUES (?, ?, ?, ?, ?)''',
+                       (user.username,
+                        user.password,
+                        user.secret_code,
+                        user.salary,
+                        user.promotion_date))
+        # Сохранение изменений
+        conn.commit()
+
+    except sqlite3.Error as e:
+        print(f"Ошибка при работе с базой данных: {e}")
+    except Exception as e:
+        print(f"Общая ошибка: {e}")
+    finally:
+        # Закрытие соединения
+        if conn:
+            conn.close()
+
+if __name__ == "__main__":
+    valid_user = User(**valid_user)
+    set_database(valid_user)
