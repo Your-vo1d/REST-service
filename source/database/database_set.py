351e4d285333f3ddb030cbd778bbfcf7ddef7885
Your-vo1d <my.vo1d.edge@gmail.com>
2024-05-27 17:22:12 +0700

    Added comments and formated code

--- source/database/database_set.py
+++ source/database/database_set.py
@@ -1,8 +1,12 @@
 import sqlite3
-from source import DATABASE_PATH
-from source.models import User
+
 from users_data import valid_user
 
+from source import DATABASE_PATH
+from source.models import User
+
+
+# Функция добавление работника в базу данных
 def set_database(user: User):
     try:
         # Подключение к базе данных
@@ -10,22 +14,28 @@ def set_database(user: User):
         cursor = conn.cursor()
 
         # Проверка существования таблицы users
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users';")
+        cursor.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='users';"
+        )
         if cursor.fetchone() is None:
             raise Exception("Таблица 'users' не существует в базе данных.")
 
         # Вставка данных первого пользователя в таблицу
-        cursor.execute('''INSERT INTO users 
+        cursor.execute(
+            """INSERT INTO users 
                         (username, password, secret_code, salary, promotion_date) 
-                        VALUES (?, ?, ?, ?, ?)''',
-                       (user.username,
-                        user.password,
-                        user.secret_code,
-                        user.salary,
-                        user.promotion_date))
+                        VALUES (?, ?, ?, ?, ?)""",
+            (
+                user.username,
+                user.password,
+                user.secret_code,
+                user.salary,
+                user.promotion_date,
+            ),
+        )
         # Сохранение изменений
         conn.commit()
-
+    # Обработка исключений
     except sqlite3.Error as e:
         print(f"Ошибка при работе с базой данных: {e}")
     except Exception as e:
@@ -34,6 +44,7 @@ def set_database(user: User):
         # Закрытие соединения
         if conn:
             conn.close()
+
 
 if __name__ == "__main__":
     valid_user = User(**valid_user)
