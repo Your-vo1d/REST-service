2c8786e711e48e8fa28bab8a2cec8a19c8c827b0
Your-vo1d <my.vo1d.edge@gmail.com>
2024-05-27 17:39:56 +0700

    Formatted code

--- source/config.py
+++ source/config.py
@@ -3,17 +3,16 @@
 import os
 import sys
 
-
 # Определение базовой директории проекта
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#Базовый URL сервиса 
+# Базовый URL сервиса
 BASE_URL = "http://127.0.0.1:8000"
 # Определение директории для данных
-DATA_DIR = os.path.join(BASE_DIR, 'source', 'data')
+DATA_DIR = os.path.join(BASE_DIR, "source", "data")
 
 # Создание директории для данных, если она не существует
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
 # Путь к базе данных
-DATABASE_PATH = os.path.join(DATA_DIR, 'users.db')
+DATABASE_PATH = os.path.join(DATA_DIR, "users.db")
